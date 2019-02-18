1d74310b2d437b1b3b5618bb889b726da3aac622
Bartosz Meglicki <bmegli@op.pl>
2019-02-18 23:23:40 +0100

    stream color/infrared depending on CLI argument
    - color for RGB, infrared for IR
    - always use native pixel format at least from one side

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /*
- * Realsense Network Hardware Video Encoder - hardware encoded UDP streaming H.264
+ * Realsense Network Hardware Video Encoder
+ * (Realsense hardware encoded UDP H.264 streaming)
  *
  * Copyright 2019 (C) Bartosz Meglicki <meglickib@gmail.com>
  *
@@ -28,6 +29,7 @@ struct input_args
 	int height;
 	int framerate;
 	int seconds;
+	bool stream_color; //true for color, infrared otherwise
 };
 
 bool main_loop(const input_args& input, rs2::pipeline& realsense, nhve *streamer);
@@ -36,16 +38,11 @@ int process_user_input(int argc, char* argv[], input_args* input, nhve_net_confi
 
 int main(int argc, char* argv[])
 {	
-	//struct nhve_net_config net_config = {IP, PORT};
 	//struct nhve_hw_config hw_config = {WIDTH, HEIGHT, FRAMERATE, DEVICE, PIXEL_FORMAT, PROFILE, BFRAMES, BITRATE};
 	//prepare NHVE Network Hardware Video Encoder
 	struct nhve_net_config net_config = {0};
 	struct nhve_hw_config hw_config = {0};
 	struct nhve *streamer;
-
-	//hw_config.pixel_format = "nv12";
-	//native format of Realsense RGB sensor is YUYV (YUY2)
-	hw_config.pixel_format = "yuyv422";
 
 	struct input_args user_input = {0};
 
@@ -73,28 +70,30 @@ int main(int argc, char* argv[])
 bool main_loop(const input_args& input, rs2::pipeline& realsense, nhve *streamer)
 {
 	const int frames = input.seconds * input.framerate;
-	int f, failed;
+	int f;
 	nhve_frame frame = {0};
 	uint8_t *color_data = NULL; //data of dummy color plane for NV12
 		
 	for(f = 0; f < frames; ++f)
 	{
 		rs2::frameset frameset = realsense.wait_for_frames();
-		//rs2::video_frame ir_frame = frameset.get_infrared_frame(1);
-		rs2::video_frame video_frame = frameset.get_color_frame();
 		
-	/*
-		if(!color_data)
+		rs2::video_frame video_frame = input.stream_color ? frameset.get_color_frame() : frameset.get_infrared_frame(1);		
+	
+		if(!input.stream_color && !color_data)
 		{   //prepare dummy color plane for NV12 format, half the size of Y
 			//we can't alloc it in advance, this is the first time we know realsense stride
-			int size = ir_frame.get_stride_in_bytes()*ir_frame.get_height()/2;
+			int size = video_frame.get_stride_in_bytes()*video_frame.get_height()/2;
 			color_data = new uint8_t[size];
 			memset(color_data, 128, size);
 		}
-	*/
-		//frame.linesize[0] = frame.linesize[1] =  ir_frame.get_stride_in_bytes();
+	
 		frame.linesize[0] =  video_frame.get_stride_in_bytes();
 		frame.data[0] = (uint8_t*) video_frame.get_data();
+		
+		//if we are streaming infrared we have 2 planes (luminance and color)
+		frame.linesize[1] = input.stream_color ? 0 : frame.linesize[0]; 
+		frame.data[1] = color_data; //dummy color plane for infrared
 
 		frame.framenumber = f;
 
@@ -108,7 +107,7 @@ bool main_loop(const input_args& input, rs2::pipeline& realsense, nhve *streamer
 	//flush the streamer by sending NULL frame
 	nhve_send_frame(streamer, NULL);
 		
-	//delete [] color_data;
+	delete [] color_data;
 
 	//all the requested frames processed?
 	return f==frames;
@@ -117,25 +116,28 @@ bool main_loop(const input_args& input, rs2::pipeline& realsense, nhve *streamer
 void init_realsense(rs2::pipeline& pipe, const input_args& input)
 {
 	rs2::config cfg;
-	// depth stream seems to be required for infrared to work
-	//cfg.enable_stream(RS2_STREAM_DEPTH, input.width, input.height, RS2_FORMAT_Z16, input.framerate);
-	//cfg.enable_stream(RS2_STREAM_INFRARED, 1, input.width, input.height, RS2_FORMAT_Y8, input.framerate);
 	
-	//native format of Realsense D415 RGV sesnor is YUYV
-	//https://github.com/IntelRealSense/librealsense/issues/3042
-	cfg.enable_stream(RS2_STREAM_COLOR, input.width, input.height, RS2_FORMAT_YUYV, input.framerate);
+	if(input.stream_color)
+		cfg.enable_stream(RS2_STREAM_COLOR, input.width, input.height, RS2_FORMAT_YUYV, input.framerate);	
+	else
+	{// depth stream seems to be required for infrared to work
+		cfg.enable_stream(RS2_STREAM_DEPTH, input.width, input.height, RS2_FORMAT_Z16, input.framerate);
+		cfg.enable_stream(RS2_STREAM_INFRARED, 1, input.width, input.height, RS2_FORMAT_Y8, input.framerate);
+	}
 
 	rs2::pipeline_profile profile = pipe.start(cfg);
 }
 
 int process_user_input(int argc, char* argv[], input_args* input, nhve_net_config *net_config, nhve_hw_config *hw_config)
 {
-	if(argc < 7)
+	if(argc < 8)
 	{
-		cerr << "Usage: " << argv[0] << " <host> <port> <width> <height> <framerate> <seconds> [device]" << endl;
+		cerr << "Usage: " << argv[0] << " <host> <port> <color/ir> <width> <height> <framerate> <seconds> [device]" << endl;
 		cerr << endl << "examples: " << endl;
-		cerr << argv[0] << " 127.0.0.1 9766 640 360 30 5" << endl;
-		cerr << argv[0] << " 127.0.0.1 9766 640 360 30 5 /dev/dri/renderD128" << endl;
+		cerr << argv[0] << " 127.0.0.1 9766 color 640 360 30 5" << endl;
+		cerr << argv[0] << " 127.0.0.1 9766 infrared 640 360 30 5" << endl;
+		cerr << argv[0] << " 127.0.0.1 9766 color 640 360 30 5 /dev/dri/renderD128" << endl;
+		cerr << argv[0] << " 127.0.0.1 9766 infrared 640 360 30 5 /dev/dri/renderD128" << endl;
 
 		return -1;
 	}
@@ -143,13 +145,25 @@ int process_user_input(int argc, char* argv[], input_args* input, nhve_net_confi
 	net_config->ip = argv[1];
 	net_config->port = atoi(argv[2]);
 	
-	hw_config->width = input->width = atoi(argv[3]);
-	hw_config->height = input->height = atoi(argv[4]);
-	hw_config->framerate = input->framerate = atoi(argv[5]);
+	input->stream_color = argv[3][0] == 'c';
 	
-	input->seconds = atoi(argv[6]);
+	//native format of Realsense RGB sensor is YUYV (YUY2, YUYV422)
+	//see https://github.com/IntelRealSense/librealsense/issues/3042
+
+	//on the other hand native format for VAAPI is nv12
+	//we will match:
+	//- Realsense RGB sensor YUYV with VAAPI YUYV422 (same format)
+	//- Realsense IR sensor Y8 with VAAPI NV12 (luminance plane with dummy color plane)
+	//this way we always have optimal format at least on one side and hardware conversion on other
+	hw_config->pixel_format = input->stream_color ? "yuyv422" : "nv12";
+
+	hw_config->width = input->width = atoi(argv[4]);
+	hw_config->height = input->height = atoi(argv[5]);
+	hw_config->framerate = input->framerate = atoi(argv[6]);
 	
-	hw_config->device = argv[7]; //NULL as last argv argument, or device path
+	input->seconds = atoi(argv[7]);
+	
+	hw_config->device = argv[8]; //NULL as last argv argument, or device path
 	
 	return 0;
 }
@@ -157,6 +171,7 @@ int process_user_input(int argc, char* argv[], input_args* input, nhve_net_confi
 int hint_user_on_failure(char *argv[])
 {
 	cerr << "unable to initalize, try to specify device e.g:" << endl << endl;
-	cerr << argv[0] << " 127.0.0.1 9766 640 360 30 5 /dev/dri/renderD128" << endl;
+	cerr << argv[0] << " 127.0.0.1 9766 color 640 360 30 5 /dev/dri/renderD128" << endl;
+	cerr << argv[0] << " 127.0.0.1 9766 infrared 640 360 30 5 /dev/dri/renderD128" << endl;
 	return -1;
 }
