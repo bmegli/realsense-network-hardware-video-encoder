4cb9795a82a41dd8ef96dddc364f516fc775e555
Bartosz Meglicki <bmegli@op.pl>
2019-02-18 22:27:59 +0100

    RGB sensor streaming
    - using YUYV (YUY2), the native RGB format of D400
    - using yuyv422 on FFmpeg VAAPI side (YUYV/YUY2)
    
    This should be the fastest way in theory, but it is difficult to tell
    without careful benchmarking

--- main.cpp
+++ main.cpp
@@ -43,9 +43,9 @@ int main(int argc, char* argv[])
 	struct nhve_hw_config hw_config = {0};
 	struct nhve *streamer;
 
-	hw_config.pixel_format = "nv12";
+	//hw_config.pixel_format = "nv12";
 	//native format of Realsense RGB sensor is YUYV (YUY2)
-	//hw_config.pixel_format = "yuyv422";
+	hw_config.pixel_format = "yuyv422";
 
 	struct input_args user_input = {0};
 
@@ -80,8 +80,10 @@ bool main_loop(const input_args& input, rs2::pipeline& realsense, nhve *streamer
 	for(f = 0; f < frames; ++f)
 	{
 		rs2::frameset frameset = realsense.wait_for_frames();
-		rs2::video_frame ir_frame = frameset.get_infrared_frame(1);
-
+		//rs2::video_frame ir_frame = frameset.get_infrared_frame(1);
+		rs2::video_frame video_frame = frameset.get_color_frame();
+		
+	/*
 		if(!color_data)
 		{   //prepare dummy color plane for NV12 format, half the size of Y
 			//we can't alloc it in advance, this is the first time we know realsense stride
@@ -89,10 +91,10 @@ bool main_loop(const input_args& input, rs2::pipeline& realsense, nhve *streamer
 			color_data = new uint8_t[size];
 			memset(color_data, 128, size);
 		}
-		
-		frame.linesize[0] = frame.linesize[1] =  ir_frame.get_stride_in_bytes();
-		frame.data[0] = (uint8_t*) ir_frame.get_data();
-		frame.data[1] = color_data;
+	*/
+		//frame.linesize[0] = frame.linesize[1] =  ir_frame.get_stride_in_bytes();
+		frame.linesize[0] =  video_frame.get_stride_in_bytes();
+		frame.data[0] = (uint8_t*) video_frame.get_data();
 
 		frame.framenumber = f;
 
@@ -106,7 +108,7 @@ bool main_loop(const input_args& input, rs2::pipeline& realsense, nhve *streamer
 	//flush the streamer by sending NULL frame
 	nhve_send_frame(streamer, NULL);
 		
-	delete [] color_data;
+	//delete [] color_data;
 
 	//all the requested frames processed?
 	return f==frames;
@@ -116,12 +118,12 @@ void init_realsense(rs2::pipeline& pipe, const input_args& input)
 {
 	rs2::config cfg;
 	// depth stream seems to be required for infrared to work
-	cfg.enable_stream(RS2_STREAM_DEPTH, input.width, input.height, RS2_FORMAT_Z16, input.framerate);
-	cfg.enable_stream(RS2_STREAM_INFRARED, 1, input.width, input.height, RS2_FORMAT_Y8, input.framerate);
+	//cfg.enable_stream(RS2_STREAM_DEPTH, input.width, input.height, RS2_FORMAT_Z16, input.framerate);
+	//cfg.enable_stream(RS2_STREAM_INFRARED, 1, input.width, input.height, RS2_FORMAT_Y8, input.framerate);
 	
 	//native format of Realsense D415 RGV sesnor is YUYV
 	//https://github.com/IntelRealSense/librealsense/issues/3042
-	//cfg.enable_stream(RS2_STREAM_COLOR, input.width, input.height, RS2_FORMAT_YUYV, input.framerate);
+	cfg.enable_stream(RS2_STREAM_COLOR, input.width, input.height, RS2_FORMAT_YUYV, input.framerate);
 
 	rs2::pipeline_profile profile = pipe.start(cfg);
 }
@@ -158,129 +160,3 @@ int hint_user_on_failure(char *argv[])
 	cerr << argv[0] << " 127.0.0.1 9766 640 360 30 5 /dev/dri/renderD128" << endl;
 	return -1;
 }
- 
-/*
-#include <stdio.h> //printf, fprintf
-#include <inttypes.h> //uint8_t
-
-#include "nhve.h"
-
-const char *IP; //e.g "127.0.0.1"
-unsigned short PORT; //e.g. 9667 
-
-const int WIDTH=640;
-const int HEIGHT=360;
-const int FRAMERATE=30;
-int SECONDS=10;
-const char *DEVICE; //NULL for default or device e.g. "/dev/dri/renderD128"
-const char *PIXEL_FORMAT="nv12"; //NULL / "" for default (NV12) or pixel format e.g. "rgb0"
-const int PROFILE=FF_PROFILE_H264_HIGH; //or FF_PROFILE_H264_MAIN, FF_PROFILE_H264_CONSTRAINED_BASELINE, ...
-const int BFRAMES=0; //max_b_frames, set to 0 to minimize latency, non-zero to minimize size
-const int BITRATE=0; //average bitrate in VBR
-
-//IP, PORT, SECONDS and DEVICE are read from user input
-
-int streaming_loop(struct nhve *streamer);
-int process_user_input(int argc, char* argv[]);
-int hint_user_on_failure(char *argv[]);
-void hint_user_on_success();
-
-int main(int argc, char* argv[])
-{
-	//get SECONDS and DEVICE from the command line
-	if( process_user_input(argc, argv) < 0 )
-		return -1;
-
-	//prepare library data
-	struct nhve_net_config net_config = {IP, PORT};
-	struct nhve_hw_config hw_config = {WIDTH, HEIGHT, FRAMERATE, DEVICE, PIXEL_FORMAT, PROFILE, BFRAMES, BITRATE};
-	struct nhve *streamer;
-
-	//initialize library with nhve_init
-	if( (streamer = nhve_init(&net_config, &hw_config)) == NULL )
-		return hint_user_on_failure(argv);
-
-	//do the actual encoding
-	int status = streaming_loop(streamer);
-
-	nhve_close(streamer);
-
-	if(status == 0)
-		hint_user_on_success();
-
-	return status;
-}
-
-int streaming_loop(struct nhve *streamer)
-{
-	struct nhve_frame frame = { 0 };
-	int frames=SECONDS*FRAMERATE, f;
-	const useconds_t useconds_per_frame = 1000000/FRAMERATE;
-
-	//we are working with NV12 because we specified nv12 pixel format
-	//when calling nhve_init, in principle we could use other format
-	//if hardware supported it (e.g. RGB0 is supported on my Intel)
-	uint8_t Y[WIDTH*HEIGHT]; //dummy NV12 luminance data
-	uint8_t color[WIDTH*HEIGHT/2]; //dummy NV12 color data
-
-	//fill with your stride (width including padding if any)
-	frame.linesize[0] = frame.linesize[1] = WIDTH;
-
-	for(f=0;f<frames;++f)
-	{
-		//prepare dummy image date, normally you would take it from camera or other source
-		memset(Y, f % 255, WIDTH*HEIGHT); //NV12 luminance (ride through greyscale)
-		memset(color, 128, WIDTH*HEIGHT/2); //NV12 UV (no color really)
-
-		//fill hve_frame with pointers to your data in NV12 pixel format
-		frame.data[0]=Y;
-		frame.data[1]=color;
-		
-		//increase the framenumber, this is mandatory for the network protocol
-		frame.framenumber = f;
-
-		//encode and send this frame
-		if( nhve_send_frame(streamer, &frame) != NHVE_OK)
-			break; //break on error
-
-		//simulate real time source (sleep according to framerate)
-	}
-
-	//flush the encoder by sending NULL frame, encode some last frames returned from hardware
-	nhve_send_frame(streamer, NULL);
-
-	//did we encode everything we wanted?
-	//convention 0 on success, negative on failure
-	return f == frames ? 0 : -1;
-}
-
-int process_user_input(int argc, char* argv[])
-{
-	if(argc < 4)
-	{
-		fprintf(stderr, "Usage: %s <ip> <port> <seconds> [device]\n", argv[0]);
-		fprintf(stderr, "\nexamples:\n");
-		fprintf(stderr, "%s 127.0.0.1 9766 10\n", argv[0]);
-		fprintf(stderr, "%s 127.0.0.1 9766 10 /dev/dri/renderD128\n", argv[0]);
-		return -1;
-	}
-	
-	IP = argv[1];
-	PORT = atoi(argv[2]);
-	SECONDS = atoi(argv[3]);
-	DEVICE=argv[4]; //NULL as last argv argument, or device path
-
-	return 0;
-}
-
-int hint_user_on_failure(char *argv[])
-{
-	fprintf(stderr, "unable to initalize, try to specify device e.g:\n\n");
-	fprintf(stderr, "%s 127.0.0.1 9766 10 /dev/dri/renderD128\n", argv[0]);
-	return -1;
-}
-void hint_user_on_success()
-{
-	printf("finished successfully\n");
-}
-*/
